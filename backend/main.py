fd050e8d26e54c859f8d2b65b669cdfbfaa92cb0
Marija Beleska <beleskamarija4@gmail.com>
2026-01-22 14:54:39 +0100

    Dockerize services and orchestrate with Docker Compose (frontend, backend, postgres)

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,12 @@ app = FastAPI(title="Movies CRUD API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "http://localhost",
+            "http://127.0.0.1",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
